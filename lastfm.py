ce9cb7209e2cb84ed143df83c85b7a95a6bf9638
Adedamola Shomoye <dshomoye@gmail.com>
2019-01-20 02:36:43 -0500

    add caching

--- lastfm.py
+++ lastfm.py
@@ -1,16 +1,25 @@
-from datetime import datetime
-import json
 import requests
+from datetime import datetime
+from dateutil.relativedelta import *
+import sys
 import os
-
+import pickle
 
 class LastFM:
     lastfm_api = "http://ws.audioscrobbler.com/2.0/"
+    key = None
+    SCROBBLE_FILE="scrobble_cache"
 
-    def __init__(self):
-        self.API_KEY=os.environ["LASTFM_API_KEY"]
+    if "LASTFM_API_KEY" in os.environ:
+        key = os.environ["LASTFM_API_KEY"]
 
-    def get_user_tracks(self, username):
+    def __init__(self,api_key=key):
+        self.API_KEY=api_key
+        if not self.API_KEY: raise ValueError("No API KEY passed to LastFM or set in env")
+        self.__scrobble_cache_date=datetime.now()
+        self.SCROBBLES_CACHE=[]
+
+    def get_user_scrobbles(self, username='sonofatailor'):
         """
         get all tracks for user.
 
@@ -19,18 +28,24 @@ class LastFM:
         returns:
             (dict) tracks: dictionary representation of user's scrobbles
         """
-        page=0
-        total_pages = 1
-        tracks={}
-        print("downloading....")
+
+        if self.__scrobble_cache_date + relativedelta(minutes=60) > datetime.now():
+            if self.SCROBBLES_CACHE:
+                return self.SCROBBLES_CACHE
+            else:
+                if self.__read_scrobbles_from_cache_file(): return self.SCROBBLES_CACHE
+        page, total_pages = 0,1
+        print("downloading...")
         while total_pages > page:
             page+=1
             t = self.__get_user_track_page(username,page)
+            self.SCROBBLES_CACHE+=t["recenttracks"]["track"]
             total_pages = int(t["recenttracks"]["@attr"]["totalPages"])
-            tracks = {**t,**tracks}
             progress=int(page/total_pages*100)
             print(f"\r {'=' * int(progress/2)}>  {progress}% done",end="")
-        return tracks
+        print("downloaded")
+        self.__write_scrobbles_to_cache_file()
+        return self.SCROBBLES_CACHE
     
     def __get_user_track_page(self,username,page):
         payload = {
@@ -40,7 +55,20 @@ class LastFM:
             "page":page
         }
         r = self.__do_request("GET",payload)
-        return r.json()
+        return r.json() 
+    
+    def __write_scrobbles_to_cache_file(self):
+        with open(self.SCROBBLE_FILE, 'wb') as output:
+            pickle.dump(self.SCROBBLES_CACHE, output, pickle.HIGHEST_PROTOCOL)
+    
+    def __read_scrobbles_from_cache_file(self):
+        try:
+            with open(self.SCROBBLE_FILE, 'rb') as input:
+                self.SCROBBLES_CACHE = pickle.load(input)
+            return True
+        except:
+            return False
+        
 
     def __do_request(self,http_method,payload):
         request_methods = {
@@ -55,5 +83,10 @@ class LastFM:
 
 if __name__=="__main__":
     lf = LastFM()
-    tracks = lf.get_user_tracks("sonofatailor")
-    print(tracks["recenttracks"]["track"][0]['name'])
+    start = datetime.now()
+    tracks = lf.get_user_scrobbles("sonofatailor")
+    end = datetime.now()
+    print(f'started at {start} and ended at {end}')
+    size = sys.getsizeof(tracks)
+    print(f'size of scrobble list is {size}')
+    print(tracks[-1])
