704722060a80e72f6d861633a538b3d2e9ea0889
Adedamola Shomoye <dshomoye@gmail.com>
2019-01-20 02:51:25 -0500

    initial logic, get top track for specifc period (as datetime)

---
+++ data.py
@@ -0,0 +1,53 @@
+from lastfm import LastFM
+from datetime import datetime
+from dateutil.relativedelta import *
+from models import Scrobble, Track
+ 
+import os
+
+
+class DataHelper:
+
+    def __init__(self):
+        self.API_KEY=os.environ["LASTFM_API_KEY"]
+        self.lf = LastFM(api_key=self.API_KEY)
+        self.SCROBBLES_CACHE=None
+        self.__scrobbles_parsed = False
+    
+    def __get_scrobbles(self, end=None):
+        if not self.SCROBBLES_CACHE:
+            self.SCROBBLES_CACHE = self.lf.get_user_scrobbles()
+        if not self.__scrobbles_parsed:
+            self.__parse_scrobbles()
+
+    def __parse_scrobbles(self):
+        parsed_scrobbles=[]
+        for scrobble in self.SCROBBLES_CACHE:
+            t = Track(title=scrobble["name"],artist=scrobble["artist"]['#text'],album=scrobble["album"]['#text'])
+            s = Scrobble(track=t,date=int(scrobble["date"]["uts"]))
+            parsed_scrobbles.append(s)
+        self.SCROBBLES_CACHE=parsed_scrobbles
+        print("parsing done")
+        self.__scrobbles_parsed=True
+
+
+    def get_top_track_for_period(self, start_period: datetime,end_period: datetime):
+        tracks = self.get_tracks_for_period(start_period,end_period)
+        print(len(tracks))
+        return max(tracks,key=tracks.count)
+
+
+    def get_tracks_for_period(self, start_period: datetime,end_period: datetime):
+        self.__get_scrobbles()
+        return list((scrobble.track for scrobble in self.SCROBBLES_CACHE if start_period <= scrobble.date <= end_period ))   
+        
+
+
+if __name__ == "__main__":
+    d = DataHelper()
+    now = datetime.now()
+    start = now+relativedelta(months=-5)
+    end = start+relativedelta(weeks=5)
+    print(f"start {start} and end {end} ")
+    a = d.get_top_track_for_period(start_period=start,end_period=end)
+    print(a)
---
+++ models.py
@@ -0,0 +1,50 @@
+from datetime import datetime
+from functools import total_ordering
+
+
+class Track:
+    def __init__(self, title: str, artist: str, album: str = None):
+        self.title = title
+        self.artist_name = artist
+        self.album_name = album
+    
+
+    def __eq__(self,other):
+        if not isinstance(other, Track): return NotImplemented
+        if ( self.title == other.title and 
+                self.album_name == other.album_name and 
+                self.artist_name == self.artist_name):
+            #only match if all attribute are same
+            return True
+        
+    def __repr__(self):
+        return f"{{Title: {self.title}, Album: {self.album_name}, Artist: {self.artist_name}  }}"
+
+
+@total_ordering
+class Scrobble:
+
+    def __init__(self,track: Track, date: int):
+        if not isinstance(track, Track): raise AttributeError("must supply a track object to create scrobble!")
+        self.track = track
+        try:
+            self.date = datetime.fromtimestamp(date)
+        except ValueError:
+            raise ValueError(f"failed to create Scrobble object")
+
+    def __lt__(self,other):
+        if not isinstance(other, Scrobble): return NotImplemented
+        return self.date < other.date
+    
+    def __eq__(self,other):
+        if not isinstance(other, Scrobble): return NotImplemented
+        return self.date == other.date and self.track == other.track
+    
+    def __repr__(self):
+        return f"{{Track: {self.track}, Date: {self.date} }}"
+    
+
+
+if __name__ == "__main__":
+    test = Track(artist="Moelogo",title="Ireti")
+    print(test)
