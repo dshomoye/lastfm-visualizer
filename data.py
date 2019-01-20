0d0b016c706c17d0a6ee60f64f8f11fb17a0592e
Adedamola Shomoye <dshomoye@gmail.com>
2019-01-20 13:56:23 -0500

    add getting arbitrary top tracks

--- data.py
+++ data.py
@@ -31,14 +31,14 @@ class DataHelper:
         self.__scrobbles_parsed=True
 
 
-    def get_top_track_for_period(self, start_period: datetime,end_period: datetime):
+    def get_top_tracks_for_period(self, start_period: datetime,end_period: datetime, number_of_tracks=1) -> list:
         start = datetime.now()
         print(f'time to get all tracks {datetime.now()-start}')
         print(f'start time of max {datetime.now()}')
-        return self.get_tracks_for_period(start_period,end_period).most_common(1)[0]
+        return self.get_tracks_for_period(start_period,end_period).most_common(number_of_tracks)
 
 
-    def get_tracks_for_period(self, start_period: datetime,end_period: datetime):
+    def get_tracks_for_period(self, start_period: datetime,end_period: datetime) -> Counter:
         self.__get_scrobbles()
         return Counter((scrobble.track for scrobble in self.SCROBBLES_CACHE if start_period <= scrobble.date <= end_period ))   
         
@@ -49,6 +49,6 @@ if __name__ == "__main__":
     now = datetime.now()
     start = now+relativedelta(months=-10)
     end = start+relativedelta(weeks=20)
-    a = d.get_top_track_for_period(start_period=start,end_period=end)
+    a = d.get_top_tracks_for_period(start_period=start,end_period=end, number_of_tracks=5)
     print(f'end time of max {datetime.now()}')
     print(a)
